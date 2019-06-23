884140511848ef3ea7ed3749a435f2486ecd48a4
David Couronné <couronne.david@gmail.com>
2019-06-23 23:35:01 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,6 +2,7 @@ const { sideBar } = require("./sidebar");
 
 module.exports = {
   lang: 'fr-FR',
+  evergreen: true,
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
@@ -46,7 +47,49 @@ module.exports = {
     plugins: [
       require("tailwindcss")("./tailwind.config.js"),
       require("autoprefixer"),
-      require('@fullhuman/postcss-purgecss')
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+            `${cwd}/.vuepress/theme/**/*.*`,
+            `${cwd}/!(node_modules)/**/*.md`,
+            './src/**/*.vue',
+    './src/**/*.js',    
+    './src/**/*.html',    
+    './src/**/*.md',
+        ],
+
+        extractors: [
+            {
+                /**
+                 * A fix for purge css to pick up class names with escaped chars
+                 * E.g. md:w-1/2.
+                 *
+                 * Solution from https://github.com/tailwindcss/tailwindcss/issues/391#issuecomment-366922730
+                 */
+                extractor: class TailwindExtractor {
+                    static extract(content) {
+                        return (
+                            content.match(/[A-z0-9-:\/]+/g) || []
+                        );
+                    }
+                },
+                extensions: [
+                    "css",
+                    "html",
+                    "js",
+                    "vue",
+                    "md",
+                    "styl"
+                ]
+            }
+        ],
+
+        /**
+         * Ensure default resets and normalised classes ar enot removed by PurgeCSS
+         */
+        whitelistPatterns: [
+            /^(h\d|p$|ul|li$|div|ol|table|td$|th$|thead|tbody|main|input|button|form|md-|hljs)/
+        ]
+    })
     ]
   }
 }
