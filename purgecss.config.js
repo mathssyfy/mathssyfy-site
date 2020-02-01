4bed02e77d8282aaa7ace12978bb978d4b19c2bc
David Couronné <couronne.david@gmail.com>
2020-02-01 08:04:54 +0100

    Update purgecss.config.js

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,31 +1,24 @@
 class TailwindExtractor {
   static extract(content) {
-    return content.match(/[A-z0-9-:\\/]+/g)
+    return content.match(/[A-z0-9-:\\/]+/g);
   }
 }
 
 module.exports = {
   content: [
-    './src/**/*.vue',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.html',
-    './src/**/*.pug',
-    './src/**/*.md',
+    "./src/**/*.vue",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.html",
+    "./src/**/*.pug",
+    "./src/**/*.md"
   ],
-  whitelist: [
-    'body',
-    'html',
-    'img',
-    'a',
-    'g-image',
-    'g-image--lazy',
-    'g-image--loaded',
-  ],
+  whitelist: ["body", "html", "img", "a"],
+  whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
   extractors: [
     {
       extractor: TailwindExtractor,
-      extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
-    },
-  ],
-}
+      extensions: ["vue", "js", "jsx", "md", "html", "pug"]
+    }
+  ]
+};
